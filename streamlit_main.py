fcf550cec54b4e48d95c7eb9b0b910529227fb30
SY9507 <suyon7097@gmail.com>
2026-02-07 12:35:02 +0000

    로또 생성기

--- streamlit_main.py
+++ streamlit_main.py
@@ -4,31 +4,17 @@ import os, time
 
 print(f"✅ {os.path.basename(__file__)} 실행됨 {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-st.title("다양한 widget들")
+import random
 
-model = st.selectbox("모델선택", ("GPT-3", "GPT-4", "GPT-5"))
+st.title(":sparkles: 로또생성기 :sparkles:")
 
-st.markdown(f"model: :green[{model}]")
+def generate_lotto():
+    lotto = [i + 1 for i in range(45)]
+    random.shuffle(lotto)
+    return lotto[:6]
 
-name = st.text_input("이름이 무엇입니까?")
-st.markdown(f"이름: :red[{name}]")
+button = st.button('로또를 생성해 주세요')
 
-
-button = st.button("버튼을 눌러보세요")
 if button:
-    st.write(":blue[버튼] 이 눌렸습니다 :sparkles:")
-
-st.markdown('---')
-num1 = st.number_input('숫자1 입력',
-    min_value=10, max_value=100, step=5)
-num2 = st.number_input('숫자2 입력',
-    min_value=10, max_value=100, step=5)
-
-btn_calc = st.button('계산을 합니다')
-
-if btn_calc:
-    st.markdown(f"""
-            {num1} + {num2} = {num1 + num2}
-            
-            {num1} - {num2} = {num1 - num2}
-            """)
+    for i in range(5):
+        st.subheader(f"{i+1}. 행운의번호: :green[{generate_lotto()}]")
